0002c2cdf00b7b04885ccf6aa79ba10b395d219d
yoshitomi5103 <yoshitomi5103@gmail.com>
2025-02-15 13:46:39 +0900

    temporary commit

--- yukidoko/src/App.tsx
+++ yukidoko/src/App.tsx
@@ -1,33 +1,49 @@
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
-//Routes、Route、Link、BrowserRouterをインポートし、ルーティング
-import Home from "./pages/Home"; // ホームページのコンポーネント
-//import GoogleMap from "./pages/GoogleMap"; // Aboutページのコンポーネント
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotLogin from "./pages/NotLogin";
 import styles from './css/App.module.css';
+
+import Snowfall from "./Snowfall";
 
 function App() {
   return (
     <BrowserRouter>
-    {/* BrowserRouterで囲みます */}
+      <Snowfall />
       <div>
-        <h1 className={styles.title}>React Router Vite デモ</h1>
+        <Link to="/" className={styles.title}>
+          ゆきどこ
+        </Link>
+
         <nav>
+          {/* ログイン・新規登録 (右上) */}
+          <div className={styles.navAuth}>
+            <Link to="/Login">ログイン</Link>
+          </div>
+
           <ul>
-            <li>
-              <Link to="/">ホーム</Link>
-            </li>
-            <li>
-              <Link to="/googleMap">GoogleMap</Link>
-            </li>
+            {/* 探す・共有する (横並び) */}
+            <div className={styles.navActions}>
+              <li>
+                <Link to="/Search">探す</Link>
+              </li>
+              <li>
+                <Link to="/NotLogin">共有する</Link>
+              </li>
+            </div>
           </ul>
         </nav>
 
-        {/* ルーティングの設定 */}
         <Routes>
-          <Route  path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/NotLogin" element={<NotLogin />} />
         </Routes>
+
       </div>
     </BrowserRouter>
   );
 }
 
 export default App;
+
