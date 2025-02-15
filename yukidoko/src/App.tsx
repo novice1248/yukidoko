65f69e36049272786bbc33901a4cc9e1e15e41bf
novice1248 <shirodora1248@gmail.com>
2025-02-15 10:31:26 +0900

    pagesの追加とrouterの設定とcssをcss.moduleに変更

--- yukidoko/src/App.tsx
+++ yukidoko/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+//Routes、Route、Link、BrowserRouterをインポートし、ルーティング
+import Home from "./pages/Home"; // ホームページのコンポーネント
+//import GoogleMap from "./pages/GoogleMap"; // Aboutページのコンポーネント
+import styles from './css/App.module.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <BrowserRouter>
+    {/* BrowserRouterで囲みます */}
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className={styles.title}>React Router Vite デモ</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">ホーム</Link>
+            </li>
+            <li>
+              <Link to="/googleMap">GoogleMap</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* ルーティングの設定 */}
+        <Routes>
+          <Route  path="/" element={<Home />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ yukidoko/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div>
+        <h2>ホームページ</h2>
+        <p>ここはホームページです！</p>
+      </div>
+    );
+  }
+  
+  export default Home;
